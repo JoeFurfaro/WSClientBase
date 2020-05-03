6857bda33a556a8f6ccb4812997359675d3db05c
= <=>
2020-05-03 16:32:17 -0400

    Implement sessions

--- example.js
+++ example.js
@@ -2,9 +2,11 @@ var wscb = new WSClientBase("localhost", 8765, false);
 
 var foo = new FooPlugin(wscb);
 var passwords = new PasswordsPlugin(wscb);
+var sessions = new SessionsPlugin(wscb);
 
 wscb.register(foo);
 wscb.register(passwords);
+wscb.register(sessions);
 
 // Define a custom plugin welecome function
 foo.on_example = function(response) {
@@ -13,7 +15,8 @@ foo.on_example = function(response) {
 
 // Define the on open function
 wscb.on_open = function() {
-    passwords.auth("joe", "password"); // <- Automatically attempt to authenticate a user named joe when connection is established
+    if(!sessions.auth())
+        passwords.auth("joe", "password")
 }
 
 // Will be called when authenticated
--- plugins/sessions.js
+++ plugins/sessions.js
@@ -12,19 +12,43 @@ class SessionsPlugin extends WSCBPlugin {
             handlers[response.code](response);
     }
 
-    auth = function(user_name, password) {
-        this.request({
-            type: "request",
-            code: "auth",
-            user_name: user_name,
-            password: password
-        });
+    has_key = function() {
+        return !($.cookie("wssb_session_id") == undefined);
+    }
+
+    has_user = function() {
+        return !($.cookie("wssb_session_user") == undefined);
+    }
+
+    get_id = function() {
+        return $.cookie("wssb_session_id");
+    }
+
+    get_user = function() {
+        return $.cookie("wssb_session_user");
+    }
+
+    auth = function() {
+        console.log("Trying to send cookie!");
+        if(this.has_key() && this.has_user()) {
+            console.log("Sending session cookie!");
+            let auth_token = {
+                type: "request",
+                code: "auth",
+                session_id: this.get_id(),
+                user_name: this.get_user(),
+            };
+            console.log(auth_token);
+            this.request(auth_token);
+            return true;
+        }
+        return false;
     }
 
     on_new_session = function(response) {
-        $(document).ready(function() {
-            $.cookie("wssb_session_id", response.session_id);
-        });
+        $.cookie("wssb_session_id", response.session_id);
+        $.cookie("wssb_session_user", response.user_name)
+        console.log($.cookie("wssb_session_id"));
     }
 
     /*
